bd50fe52d6ddb0ed65e50890b1f7622f79424cb0
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-02-14 23:59:19 -0300

    Mais alguns exercícios

---
+++ exercises/4_1/produto.js
@@ -0,0 +1,7 @@
+let custo = 100
+custoimposto = (custo * 0.2) + custo //imposto de 20%
+console.log("Custo com imposto: " + custoimposto)
+const valor_venda = 50
+const lucro = (valor_venda * 1000) - custoimposto
+
+console.log(lucro)
