7be0db6da4e1f28d3b284b94dcc2a491f21065c4
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-02-28 14:39:32 -0300

    comit

--- projetos/meme-generator/script.js
+++ projetos/meme-generator/script.js
@@ -1,8 +1,17 @@
 const img_input = document.getElementById('input_img');
-const memetext = document.getElementById('text');
+const memetextdown = document.getElementById('textdown');
+const memetextup = document.getElementById('textup');
 
 const imgbox = document.getElementById('imgbox');
 
 img_input.addEventListener ('change', function(){ 
     document.getElementById('image').src = window.URL.createObjectURL(this.files[0]);
+});
+
+memetextdown.addEventListener('keyup', function(){
+    document.getElementById('memedown').innerText = memetextdown.value;
+});
+
+memetextup.addEventListener('keyup', function(){
+    document.getElementById('memeup').innerText = memetextup.value;
 });
