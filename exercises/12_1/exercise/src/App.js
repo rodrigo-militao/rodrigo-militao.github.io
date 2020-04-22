60e5445feac4bedd48ef51699a48775de342171a
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-22 19:58:37 -0300

    showing list

--- exercises/12_1/exercise/src/App.js
+++ exercises/12_1/exercise/src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const task = (value) => {
+    return (
+      <li>{value}</li>
+    );
+  }
+  const compromises = ['Soccer', 'Reading', 'Studying', 'Preparing for class'];
   return (
     <div className="App">
-      
+      {compromises.map((el) => task(el))}
     </div>
   );
 }
