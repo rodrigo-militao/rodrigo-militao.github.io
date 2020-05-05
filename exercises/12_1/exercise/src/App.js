bc05a16baec995ee1d1cc2a542af94d6634a78d5
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-05-05 15:19:02 -0300

    changes

--- exercises/12_1/exercise/src/App.js
+++ exercises/12_1/exercise/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const task = (value) => {
-    return (
-      <li>{value}</li>
-    );
-  }
   const compromises = ['Soccer', 'Reading', 'Studying', 'Preparing for class'];
+  const task = (value) => {
+    return <li> {value} </li>
+  }
+
   return (
     <div className="App">
-      {compromises.map((el) => task(el))}
+      <h2>Lista de Tarefas</h2>
+         
+      <ul>
+        {compromises.map((el) => task(el))}
+      </ul>
     </div>
   );
 }
